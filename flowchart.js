61ba366f507054759ed7ae22ebaefd5fc1fdbec4
Voon <macbookair@Macbooks-MacBook-Air.local>
2019-04-14 12:47:36 +0800

    First commit

---
+++ flowchart.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+  var $flowchart = $("#example");
+  var $container = $flowchart.parent();
+
+  var cx = $flowchart.width() / 2;
+  var cy = $flowchart.height() / 2;
+
+  // Panzoom initialization...
+  $flowchart.panzoom();
+
+  // Centering panzoom
+  $flowchart.panzoom(
+    "pan",
+    -cx + $container.width() / 2,
+    -cy + $container.height() / 2
+  );
+
+  // Panzoom zoom handling...
+  var possibleZooms = [0.5, 0.75, 1, 2, 3];
+  var currentZoom = 2;
+  $container.on("mousewheel.focal", function(e) {
+    e.preventDefault();
+    var delta = e.delta || e.originalEvent.wheelDelta || e.originalEvent.detail;
+    var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+    currentZoom = Math.max(
+      0,
+      Math.min(possibleZooms.length - 1, currentZoom + (zoomOut * 2 - 1))
+    );
+    $flowchart.flowchart("setPositionRatio", possibleZooms[currentZoom]);
+    $flowchart.panzoom("zoom", possibleZooms[currentZoom], {
+      animate: false,
+      focal: e
+    });
+  });
+
+  var data = {};
+
+  // Apply the plugin on a standard, empty div...
+  $flowchart.flowchart({
+    data: data,
+    linkWidth: 5
+  });
+
+  $flowchart
+    .parent()
+    .siblings(".delete_selected_button")
+    .click(function() {
+      $flowchart.flowchart("deleteSelected");
+    });
+
+  var $draggableOperators = $(".draggable_operator");
+
+  //This is where operatorData is retrieved
+  function getOperatorData($element) {
+    var nbInputs = parseInt($element.data("nb-inputs"));
+    var nbOutputs = parseInt($element.data("nb-outputs"));
+    var shape = $element.data("shape");
+    var data = {
+      properties: {
+        title: $element.text(),
+        inputs: {},
+        outputs: {},
+        shape: shape
+      }
+    };
+
+    var i = 0;
+    for (i = 0; i < nbInputs; i++) {
+      data.properties.inputs["input_" + i] = {
+        label: ""
+      };
+    }
+    for (i = 0; i < nbOutputs; i++) {
+      data.properties.outputs["output_" + i] = {
+        label: ""
+      };
+    }
+
+    return data;
+  }
+
+  var operatorId = 0;
+
+  $draggableOperators.draggable({
+    cursor: "move",
+    opacity: 0.7,
+
+    helper: "clone",
+    appendTo: "body",
+    zIndex: 1000,
+
+    helper: function(e) {
+      var $this = $(this);
+      var data = getOperatorData($this);
+      console.log("operatorDataSource", data);
+      //This is where the operatorData is passed into the flowchart function
+      return $flowchart.flowchart("getOperatorElement", data);
+    },
+    stop: function(e, ui) {
+      var $this = $(this);
+      var elOffset = ui.offset;
+      var containerOffset = $container.offset();
+      if (
+        elOffset.left > containerOffset.left &&
+        elOffset.top > containerOffset.top &&
+        elOffset.left < containerOffset.left + $container.width() &&
+        elOffset.top < containerOffset.top + $container.height()
+      ) {
+        var flowchartOffset = $flowchart.offset();
+
+        var relativeLeft = elOffset.left - flowchartOffset.left;
+        var relativeTop = elOffset.top - flowchartOffset.top;
+
+        var positionRatio = $flowchart.flowchart("getPositionRatio");
+        relativeLeft /= positionRatio;
+        relativeTop /= positionRatio;
+
+        var data = getOperatorData($this);
+        data.left = relativeLeft;
+        data.top = relativeTop;
+
+        $flowchart.flowchart("addOperator", data);
+      }
+    }
+  });
+});
