f55307c439e097b0342b149dfb0a78150b525590
mingsterism <ming.k@hotmail.com>
2019-04-30 16:21:56 +0800

    bug files

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: require.resolve("jquery"),
+//         use: "imports-loader?$=jquery!./flowchart/jquery.flowchart.js"
+//       }
+//     ]
+//   }
+// };
