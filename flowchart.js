173158e745b566c49e42029ffdbcd0ff1ca95a78
codetosanfrancisco <codetosanfrancisco@gmail.com>
2019-06-22 01:19:13 +0800

    Flowchart.js factorised

--- flowchart.js
+++ flowchart.js
@@ -2,37 +2,30 @@ import { EventEmitter } from "events";
 
 // var $ = require("./node_modules/jquery/dist/jquery");
 // var panzoom = require("jquery.panzoom");
+function int(string) {
+  return parseInt(string);
+}
 
-$(document).ready(function() {
-  var $flowchart = $("#example");
-  console.log($flowchart, "===============");
-  console.log($flowchart.flowchart);
-  var $container = $flowchart.parent();
+function generateRandomString() {
+  return Math.random()
+    .toString(36)
+    .substring(7);
+}
 
-  var cx = $flowchart.width() / 2;
-  var cy = $flowchart.height() / 2;
-
-  // Panzoom initialization...
-  // just grab a DOM element
-  var element = document.querySelector("#example");
-  // console.log(element, '=========')
-  // And pass it to panzoom
-
-  panzoom(element);
-
-  $flowchart.panzoom();
-
-  // // Centering panzoom
-  $flowchart.panzoom(
-    "pan",
-    -cx + $container.width() / 2,
-    -cy + $container.height() / 2
-  );
-
-  // Panzoom zoom handling...
+function panzoomHandler(flowchartElement, flowchartElementJS, container) {
+  var cx = flowchartElement.width() / 2;
+  var cy = flowchartElement.height() / 2;
+  var centerX = -cx + container.width() / 2;
+  var centerY = -cy + container.height() / 2;
   var possibleZooms = [0.5, 0.75, 1, 2, 3];
   var currentZoom = 2;
-  $container.on("mousewheel.focal", function(e) {
+
+  panzoom(flowchartElementJS);
+
+  flowchartElement.panzoom("pan", centerX, centerY);
+
+  // Panzoom zoom handling...
+  container.on("mousewheel.focal", function(e) {
     e.preventDefault();
     var delta = e.delta || e.originalEvent.wheelDelta || e.originalEvent.detail;
     var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
@@ -40,99 +33,85 @@ $(document).ready(function() {
       0,
       Math.min(possibleZooms.length - 1, currentZoom + (zoomOut * 2 - 1))
     );
-    $flowchart.flowchart("setPositionRatio", possibleZooms[currentZoom]);
-    $flowchart.panzoom("zoom", possibleZooms[currentZoom], {
+    flowchartElement.flowchart("setPositionRatio", possibleZooms[currentZoom]);
+    flowchartElement.panzoom("zoom", possibleZooms[currentZoom], {
       animate: false,
       focal: e
     });
   });
+}
 
-  var data = {};
-
-  // Apply the plugin on a standard, empty div...
-  $flowchart.flowchart({
-    data: data,
-    linkWidth: 5
-  });
-  $flowchart
+function deleteOperatorHandler(flowchartElement) {
+  flowchartElement
     .parent()
     .siblings()
     .children(".delete_selected_button")
     .click(function() {
       console.log("DELETING");
-      $flowchart.flowchart("deleteSelected");
+      flowchartElement.flowchart("deleteSelected");
     });
+}
 
-  var $draggableOperators = $(".draggable_operator");
+function initializeFlowChart(flowchartElement, data) {
+  flowchartElement.flowchart({
+    data,
+    linkWidth: 5
+  });
+}
 
-  //This is where operatorData is retrieved
-  function getOperatorData($element) {
-    var nbInputs = parseInt($element.data("nb-inputs"));
-    var nbOutputs = parseInt($element.data("nb-outputs"));
-    var shape = $element.data("shape");
-    var func = $element.data("function");
-    let r =
-      func == "decider"
-        ? Math.random()
-            .toString(36)
-            .substring(7)
-        : null;
-    var data = {
-      properties: {
-        title: $element.text(),
-        inputs: {},
-        outputs: {},
-        shape: shape,
-        func: func,
-        random: r,
-        objectId: ""
-      }
+function setInAndOutAttribute(dataObject) {
+  const { data, nbInputs, nbOutputs } = dataObject;
+  var i = 0;
+  for (i = 0; i < nbInputs; i++) {
+    data.properties.inputs["input_" + i] = {
+      label: ""
     };
-
-    var i = 0;
-    for (i = 0; i < nbInputs; i++) {
-      data.properties.inputs["input_" + i] = {
-        label: ""
-      };
-    }
-    for (i = 0; i < nbOutputs; i++) {
-      data.properties.outputs["output_" + i] = {
-        label: ""
-      };
-    }
-
-    return data;
+  }
+  for (i = 0; i < nbOutputs; i++) {
+    data.properties.outputs["output_" + i] = {
+      label: ""
+    };
   }
 
-  function createApproveDeleteOperator(nbInputs, nbOutputs, title, random) {
-    var data = {
-      properties: {
-        title: title,
-        inputs: {},
-        outputs: {},
-        shape: "rectangle",
-        random: random
-      }
-    };
+  return data;
+}
 
-    var i = 0;
-    for (i = 0; i < nbInputs; i++) {
-      data.properties.inputs["input_" + i] = {
-        label: ""
-      };
+function getOperatorData(element) {
+  var nbInputs = int(element.data("nb-inputs"));
+  var nbOutputs = int(element.data("nb-outputs"));
+  var shape = element.data("shape");
+  var func = element.data("function");
+  let random = func === "decider" ? generateRandomString() : null;
+  var data = {
+    properties: {
+      title: element.text(),
+      inputs: {},
+      outputs: {},
+      shape,
+      func,
+      random,
+      objectId: ""
     }
-    for (i = 0; i < nbOutputs; i++) {
-      data.properties.outputs["output_" + i] = {
-        label: ""
-      };
+  };
+  return setInAndOutAttribute({ data, nbInputs, nbOutputs });
+}
+
+function createApproveDeleteOperator(title, random) {
+  var data = {
+    properties: {
+      title: title,
+      inputs: {},
+      outputs: {},
+      shape: "rectangle",
+      random: random
     }
+  };
 
-    return data;
-  }
+  return setInAndOutAttribute({ data, nbInputs: 1, nbOutputs: 1 });
+}
 
-  var operatorId = 0;
-
-  $draggableOperators.draggable({
+function dragHandler(draggableOperators, flowchartElement, container) {
+  draggableOperators.draggable({
     cursor: "move",
     opacity: 0.7,
 
@@ -145,7 +124,7 @@ $(document).ready(function() {
       var data = getOperatorData($this);
       console.log("operatorDataSource", data);
       //This is where the operatorData is passed into the flowchart function
-      return $flowchart.flowchart("getOperatorElement", data);
+      return flowchartElement.flowchart("getOperatorElement", data);
     },
     stop: function(e, ui) {
       console.log("!!!!!!!!!!!!!!");
@@ -154,15 +133,15 @@ $(document).ready(function() {
       var $this = $(this);
       var elOffset = ui.offset;
       // console.log(ui.offset);
-      var containerOffset = $container.offset();
-      // console.log(containerOffset);
-      if (
+      var containerOffset = container.offset();
+      var withinContainer =
         elOffset.left > containerOffset.left &&
         elOffset.top > containerOffset.top &&
-        elOffset.left < containerOffset.left + $container.width() &&
-        elOffset.top < containerOffset.top + $container.height()
-      ) {
-        var flowchartOffset = $flowchart.offset();
+        elOffset.left < containerOffset.left + container.width() &&
+        elOffset.top < containerOffset.top + container.height();
+      // console.log(containerOffset);
+      if (withinContainer) {
+        var flowchartOffset = flowchartElement.offset();
 
         var relativeLeft = elOffset.left - flowchartOffset.left;
         // console.log(flowchartOffset.left);
@@ -171,35 +150,32 @@ $(document).ready(function() {
         var relativeTop = elOffset.top - flowchartOffset.top;
         // console.log("flowchartOffset.top", flowchartOffset.top);
         // console.log(relativeTop);
+        const objectId = ObjectID().str;
 
-        var positionRatio = $flowchart.flowchart("getPositionRatio");
+        var positionRatio = flowchartElement.flowchart("getPositionRatio");
         relativeLeft /= positionRatio;
         relativeTop /= positionRatio;
 
         var data = getOperatorData($this);
         data.left = relativeLeft;
         data.top = relativeTop;
-        data.properties.objectId = ObjectID().str;
-        const objectId = data.properties.objectId;
-        console.log("Emitting event: nodeCreated with objectId: ", objectId)
-        const nodeCreated = new Event('nodeCreated', {objectId})
+        data.properties.objectId = objectId;
+
+        console.log("Emitting event: nodeCreated with objectId: ", objectId);
+        const nodeCreated = new Event("nodeCreated", { objectId });
         window.dispatchEvent(nodeCreated);
 
         // console.log(relativeLeft, relativeTop);
 
         //This function comes from the library
-        $flowchart.flowchart("addOperator", data);
+        flowchartElement.flowchart("addOperator", data);
 
         if (data.properties.func == "decider") {
           var approve = createApproveDeleteOperator(
-            1,
-            1,
             "Approve",
             data.properties.random
           );
           var reject = createApproveDeleteOperator(
-            1,
-            1,
             "Reject",
             data.properties.random
           );
@@ -207,10 +183,25 @@ $(document).ready(function() {
           approve.top = relativeTop + 100;
           reject.left = relativeLeft + 140;
           reject.top = approve.top;
-          $flowchart.flowchart("addOperator", approve);
-          $flowchart.flowchart("addOperator", reject);
+          flowchartElement.flowchart("addOperator", approve);
+          flowchartElement.flowchart("addOperator", reject);
         }
       }
     }
   });
+}
+$(document).ready(function() {
+  var $flowchart = $("#example");
+  var $element = document.querySelector("#example");
+  var $container = $flowchart.parent();
+  var data = {};
+  var $draggableOperators = $(".draggable_operator");
+
+  initializeFlowChart($flowchart, data);
+  panzoomHandler($flowchart, $element, $container);
+  deleteOperatorHandler($flowchart);
+  dragHandler($draggableOperators, $flowchart, $container);
+  // Apply the plugin on a standard, empty div...
+
+  //This is where operatorData is retrieved
 });
